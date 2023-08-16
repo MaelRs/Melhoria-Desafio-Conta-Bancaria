0dfe8b0e5bb347087729b467becc47b9ea4451f1
MaelRs <robertdossantos.ismael@gmail.com>
2023-08-16 10:03:39 -0300

    LAST VERSION SISTEMA BANCÁRIO

--- ContaBancaria.py
+++ ContaBancaria.py
@@ -28,58 +28,56 @@ def depositar(saldo,valor_deposito, extrato,/):
     if valor_deposito>0:
         saldo+=valor_deposito
         extrato+=f'Depósito:\t\tR${valor_deposito:.2f}\n'
-        print(f'\nDepósito de R${valor_deposito:.2f} efetuado com sucesso!')
+        print(f'\033[1;30;42m Depósito de R${valor_deposito:.2f} efetuado com sucesso!\033[m')
     else:
-        print('\033[1;30;41 OPERAÇÂO FALHOU! O Valor informado é Inválido! \033[m')
+        print('\033[1;30;41m OPERAÇÂO FALHOU! O Valor informado é Inválido! \033[m')
 
     return saldo,extrato
 
 
-def sacar(*,saldo, valor_saque,extrato,limite_saque,numero_saques,limite,total_saques):
+def sacar(*,saldo, valor_saque,extrato,limite,numero_saques,limite_saques):
     if valor_saque>saldo:
         print('\033[1;30;41m Operação falhou!Valor informado excede o saldo disponível para saque.\033[m')
     elif valor_saque<=0:
-        print('\033[1;30;41m Valor Inválido! INFORME UM VALOR VÁLIDO! \033[m')
+        print('\033[1;30;41m VALOR INVÁLIDO! INFORME UM VALOR VÁLIDO! \033[m')
     elif valor_saque>limite:
         print('\033[1;30;41m Operação falhou!Valor informado excede o limite para saque.\033[m')
-    elif numero_saques>=limite_saque:
+    elif numero_saques>limite_saques:
         print('\033[1;30;41m Operação falhou!Você excedeu o número diário para saques.')
     else:
         print(f'\033[1;31;42m Saque de R${valor_saque:.2f} efetuado com sucesso! \033[m')
         numero_saques+=1
         saldo-=valor_saque
-        total_saques+=valor_saque
-        extrato+= f'Saque:\t\tR$ {valor_saque:.2f}\n'
+        extrato+= f'Saque:\t\t\tR$ {valor_saque:.2f}\n'
     return saldo, extrato
    
 
-def extrato(total_depositos,total_saques,saldo):
+def exibir_extrato(saldo,/,*,extrato):
     linha()
     print('Extrato de Conta Bancária'.center(50))
     linha()
     print('Não foram realizadas movimentações.'if not extrato else extrato)
-    print(f'''\n{extrato}
-            \nValor total de Depósitos:\tR${total_depositos:.2f}
-            \nValor total de saques:\tR${total_saques:.2f}
-            \nSaldo Disponível: \tR${saldo:.2f}''')
+    print(f'\nSaldo Disponível: \tR${saldo:.2f}''')
     sleep(2)
+
+
 def criar_usuario(usuarios):
     cpf=input('Informe o CPF(Somente números): ')
     usuario=filtrar_usuario(cpf,usuarios)
-
     if usuario:
         print('\n Já existe usuário com este CPF!')
         return
     nome=input('Informe o Nome completo: ')
     data_nascimento=input('Infome a data de nascimento (dd-mm-aaaa): ')
     endereço=input('Infome o endereço(logradouro,n°-bairro-Cidade/sigla estado:): ')
-
     usuarios.append({'nome':nome,'data_nascimento':data_nascimento,'cpf':cpf,'endereço': endereço})
     print('\033[1;30;41 Usuário criado com sucesso!!\033[m')
+
 
 def filtrar_usuario(cpf,usuarios):
     usuarios_filtratdos=[usuario for usuario in usuarios if usuario['cpf']==cpf]
     return usuarios_filtratdos[0] if usuarios_filtratdos else None
+
 
 def criar_conta(agencia,numero_conta,usuarios):
     cpf=input('Informe o CPF do usuário: ')
@@ -106,17 +104,17 @@ def sair():
     linha()
     print('\033[1;31;42m Obrigado por usar os serviços do Banco Ismael!\n Até a próxima.\033[m')
     linha()
+    
+
 
 def main():
     LIMITE_SAQUES=3
     AGENCIA='0001'
-    numero_conta=0
-    numero_saques=0
-    total_saques=0
-    total_depositos=0
+    
     extrato=''
     saldo=0
     limite=500
+    numero_saques=0
     usuarios=[]
     contas=[]
     while True:
@@ -132,23 +130,24 @@ def main():
                                 extrato=extrato,
                                 limite=limite,
                                 numero_saques=numero_saques,
-                                limite_saque=LIMITE_SAQUES,
-                                total_saques=total_saques
+                                limite_saques=LIMITE_SAQUES
                                 )
         elif opcao==3:
-            extrato(saldo,extrato=extrato)
+            exibir_extrato(saldo,extrato=extrato)
         elif opcao==4:
             criar_usuario(usuarios)
         elif opcao==5:
-            criar_conta=len(contas)+1
+            numero_conta=len(contas)+1
             conta=criar_conta(AGENCIA,numero_conta,usuarios)
             if conta:
                 contas.append(conta)
         elif opcao==6:
-            listar_contas()            
+            listar_contas(contas)            
 
         elif opcao==7:
-            print('\033[1;31;42m Obrigado por usar os serviços do Banco Ismael!\n Até a próxima.\033[m')
+            sair()
+            #print('\033[1;31;42m Obrigado por usar os serviços do Banco Ismael!\n Até a próxima.\033[m')
+            break
         else:
             print('\033[1;30;41m Opção inválida! Tente novamente.\033[m')
 
